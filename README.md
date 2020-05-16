244a9cf68ff892fbdec1fb89ba1ecac8a00eea7e
Diego Leonardo Urban <diego.urban88@gmail.com>
2020-05-16 17:59:29 -0300

    jep 102 process api

--- README.md
+++ README.md
@@ -89,3 +89,8 @@ https://openjdk.java.net/jeps/110
 Nova API HTTP que implementa HTTP/2 e WebSocket que pode substituir a API HttpURLConnection.
 A API estará disponível no modulo incubator.
 
+## JEP 102: Process API Updates
+
+https://openjdk.java.net/jeps/102
+
+Aprimorado a API para controlar e gerenciar processos do sistema operacional.
---
+++ src/main/java/urban/sandbox/java9/jep102/Jep102.java
@@ -0,0 +1,43 @@
+package urban.sandbox.java9.jep102;
+
+import java.io.IOException;
+import java.lang.ProcessHandle.Info;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Optional;
+
+public class Jep102 {
+
+	public static void main(final String[] args) throws IOException, InterruptedException {
+        final ProcessHandle self = ProcessHandle.current();
+        final long PID = self.pid();
+        final Info procInfo = self.info();
+
+        final Optional<String[]> procArgs = procInfo.arguments();
+        final Optional<String> cmd = procInfo.commandLine();
+        final Optional<Instant> startTime = procInfo.startInstant();
+        final Optional<Duration> cpuUsage = procInfo.totalCpuDuration();
+
+        System.out.println("PID = " + PID);
+        System.out.println("Args = " + procArgs);
+        System.out.println("CMD = " + cmd);
+        System.out.println("Start Time = " + startTime);
+        System.out.println("CPU Usage = " + cpuUsage);
+
+        final Process geditProcess = new ProcessBuilder("gedit").start();
+        System.out.println("gedit PID = " + geditProcess.pid());
+        System.out.println("gedit isalive = " + geditProcess.isAlive());
+        System.out.println("gedit pareint pid = " + geditProcess.toHandle().parent().get().pid());
+
+        Thread.sleep(5000);
+        
+        self.children().forEach(child -> {
+            System.out.println("killing child pid = " + child.pid());
+            boolean destroyed = child.destroy();
+            System.out.println("Destroyed? = " + destroyed);
+        });
+
+        System.out.println("gedit isalive = " + geditProcess.isAlive());
+    }
+    
+}
